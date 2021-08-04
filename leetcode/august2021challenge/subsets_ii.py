fdb7f8b8daafbac82d4f48b511d57ec63878eeda
Hendry Prasetya <mhendryp99@gmail.com>
2021-08-04 13:21:36 +0700

    add subsets_ii in py leetcode w1 aug2021

---
+++ leetcode/august2021challenge/subsets_ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        set_size = len(nums)
+        pow_set_size = pow(2, set_size)
+        
+        otp = []
+        otp_set = set()
+        for counter in range(pow_set_size):
+            res = []
+            key = ""
+            for j in range(set_size):
+                if((counter & (1 << j)) > 0):
+                    res.append(nums[j])
+                    key += str(nums[j]) + "."
+                    
+            if key in otp_set:
+                pass
+            else:
+                otp_set.add(key)
+                otp.append(res)
+        
+        return otp
